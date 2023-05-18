8f724bcccb1a5b017a7799a1cd4b9ad7468fa9ce
unknown <sergijbrovko@gmail.com>
2023-05-18 18:52:40 +0300

    Data Update2

--- src/app.js
+++ src/app.js
@@ -1,17 +1,7 @@
-/** 
- Plan
- 0. Explain existing code
- 1. Listen tool changes and save in variables.
- 3. Draw shape on click.
-  - draw on canvas
-  - draw shapes
- 4. Render future shape on hover.
-*/
-
 const state = {
   color: "#7e22c9",
   size: 50,
-  shape: "square",
+  shape: "triangle",
 };
 
 const toolbarElement = document.querySelector("#toolbar");
@@ -53,11 +43,64 @@ canvasElement.addEventListener("click", (event) => {
 
   if (state.shape === "circle") {
     ctx.beginPath();
-    // PI rad === 180 deg
-    // 360 = 2 * PI rad
-    // x rad = 2 * PI / 360 * y deg
     ctx.arc(x, y, halfSize, 0, 2 * Math.PI);
     ctx.fillStyle = state.color;
     ctx.fill();
   }
+
+  if (state.shape === "triangle") {
+    ctx.beginPath();
+    ctx.moveTo(x - halfSize, y + halfSize);
+    ctx.lineTo(x + halfSize, y + halfSize);
+    ctx.lineTo(x, y - halfSize);
+    ctx.closePath();
+    ctx.fillStyle = state.color;
+    ctx.fill();
+  }
+
+  if (state.shape === "text") {
+    const textInput = document.querySelector("#text-input");
+    const text = textInput.value;
+    ctx.fillStyle = state.color;
+    ctx.font = state.size + "px Arial";
+    ctx.fillText(text, x, y);
+  }
+});
+
+// Создаем ссылку на кнопку сохранения
+const saveButton = document.querySelector("#save-button");
+
+// Обработчик события нажатия на кнопку сохранения
+saveButton.addEventListener("click", () => {
+  // Создаем ссылку для скачивания
+  const downloadLink = document.createElement("a");
+
+  // Создание нового холста с заданным цветом фона
+  const newCanvas = document.createElement("canvas");
+  newCanvas.width = canvasElement.width;
+  newCanvas.height = canvasElement.height;
+  const newCtx = newCanvas.getContext("2d");
+  newCtx.fillStyle = "#ffffff"; // Задайте нужный цвет фона
+  newCtx.fillRect(0, 0, newCanvas.width, newCanvas.height);
+
+  // Копирование содержимого текущего холста на новый холст
+  newCtx.drawImage(canvasElement, 0, 0);
+
+  // Преобразуем содержимое нового холста в URL-адрес данных
+  const imageDataURL = newCanvas.toDataURL("image/png"); // или "image/jpeg"
+
+  // Устанавливаем URL-адрес данных в атрибут href ссылки
+  downloadLink.href = imageDataURL;
+
+  // Устанавливаем имя файла для сохранения
+  downloadLink.download = "canvas_image.png"; // или "canvas_image.jpg"
+
+  // Добавляем ссылку на страницу
+  document.body.appendChild(downloadLink);
+
+  // Нажимаем на ссылку для начала скачивания
+  downloadLink.click();
+
+  // Удаляем ссылку из DOM
+  document.body.removeChild(downloadLink);
 });
--- src/dist/app.c328ef1a.js
+++ src/dist/app.c328ef1a.js
@@ -118,20 +118,10 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
-/** 
- Plan
- 0. Explain existing code
- 1. Listen tool changes and save in variables.
- 3. Draw shape on click.
-  - draw on canvas
-  - draw shapes
- 4. Render future shape on hover.
-*/
-
 var state = {
   color: "#7e22c9",
   size: 50,
-  shape: "square"
+  shape: "triangle"
 };
 var toolbarElement = document.querySelector("#toolbar");
 toolbarElement.addEventListener("input", function (event) {
@@ -161,13 +151,64 @@ canvasElement.addEventListener("click", function (event) {
   }
   if (state.shape === "circle") {
     ctx.beginPath();
-    // PI rad === 180 deg
-    // 360 = 2 * PI rad
-    // x rad = 2 * PI / 360 * y deg
     ctx.arc(x, y, halfSize, 0, 2 * Math.PI);
     ctx.fillStyle = state.color;
     ctx.fill();
   }
+  if (state.shape === "triangle") {
+    ctx.beginPath();
+    ctx.moveTo(x - halfSize, y + halfSize);
+    ctx.lineTo(x + halfSize, y + halfSize);
+    ctx.lineTo(x, y - halfSize);
+    ctx.closePath();
+    ctx.fillStyle = state.color;
+    ctx.fill();
+  }
+  if (state.shape === "text") {
+    var textInput = document.querySelector("#text-input");
+    var text = textInput.value;
+    ctx.fillStyle = state.color;
+    ctx.font = state.size + "px Arial";
+    ctx.fillText(text, x, y);
+  }
+});
+
+// Создаем ссылку на кнопку сохранения
+var saveButton = document.querySelector("#save-button");
+
+// Обработчик события нажатия на кнопку сохранения
+saveButton.addEventListener("click", function () {
+  // Создаем ссылку для скачивания
+  var downloadLink = document.createElement("a");
+
+  // Создание нового холста с заданным цветом фона
+  var newCanvas = document.createElement("canvas");
+  newCanvas.width = canvasElement.width;
+  newCanvas.height = canvasElement.height;
+  var newCtx = newCanvas.getContext("2d");
+  newCtx.fillStyle = "#ffffff"; // Задайте нужный цвет фона
+  newCtx.fillRect(0, 0, newCanvas.width, newCanvas.height);
+
+  // Копирование содержимого текущего холста на новый холст
+  newCtx.drawImage(canvasElement, 0, 0);
+
+  // Преобразуем содержимое нового холста в URL-адрес данных
+  var imageDataURL = newCanvas.toDataURL("image/png"); // или "image/jpeg"
+
+  // Устанавливаем URL-адрес данных в атрибут href ссылки
+  downloadLink.href = imageDataURL;
+
+  // Устанавливаем имя файла для сохранения
+  downloadLink.download = "canvas_image.png"; // или "canvas_image.jpg"
+
+  // Добавляем ссылку на страницу
+  document.body.appendChild(downloadLink);
+
+  // Нажимаем на ссылку для начала скачивания
+  downloadLink.click();
+
+  // Удаляем ссылку из DOM
+  document.body.removeChild(downloadLink);
 });
 },{}],"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
